0cf82d9f11ab22fa243a8cdad41815ea4d12d2db
JSanchez78657 <joel.sanchez78657@gmail.com>
2022-09-09 13:52:22 -0700

    Did the logic for building the sets of views required for teams.

--- app/src/main/java/com/example/battleconlifetracker/layouts/DynamicGameScreen.kt
+++ app/src/main/java/com/example/battleconlifetracker/layouts/DynamicGameScreen.kt
@@ -2,15 +2,17 @@ package com.example.battleconlifetracker.layouts
 
 import android.os.Bundle
 import android.view.View
-import android.view.ViewGroup
-import android.widget.ImageButton
-import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.constraintlayout.widget.ConstraintSet
 import com.example.battleconlifetracker.R
+import com.example.battleconlifetracker.model.Team
 
 class DynamicGameScreen : AppCompatActivity() {
+
+    //ViewId hashes to the tuple defining the team and player index within that team.
+    val viewsToPlayerIndex: HashMap<Int, Pair<Int, Int>> = hashMapOf()
+    val teamMap: HashMap<Int, Team> = hashMapOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,5 +44,31 @@ class DynamicGameScreen : AppCompatActivity() {
 
         constraints.applyTo(constraintLayout)
         setContentView(constraintLayout)
+    }
+
+    private fun buildTeamGame(parent: ConstraintLayout, teamAPlayerCount: Int, teamBPlayerCount: Int) {
+        var teamA = buildTeam(parent, teamAPlayerCount)
+        var teamB = buildTeam(parent, teamBPlayerCount)
+        teamA.rotation = 90.0f
+        teamB.rotation = -90.0f
+    }
+
+    private fun buildTeam(parent: ConstraintLayout, teamSize: Int): ConstraintLayout {
+        var teamFrame = ConstraintLayout(parent.context)
+        var teamForce = layoutInflater.inflate(R.layout.force_block, teamFrame, false)
+        val teamId = View.generateViewId()
+        var healthTrackers: MutableList<View> = mutableListOf()
+        var idHold: Int
+        teamFrame.addView(teamForce)
+        teamForce.id = teamId
+        teamMap[teamId] = Team(teamSize)
+        for(i in 0 until teamSize) {
+            idHold = View.generateViewId()
+            healthTrackers.add(i, layoutInflater.inflate(R.layout.health_block, teamFrame, false))
+            healthTrackers[i].id = idHold
+            viewsToPlayerIndex[idHold] = Pair(teamId, i)
+            teamFrame.addView(healthTrackers[i])
+        }
+        return teamFrame
     }
 }
--- app/src/main/java/com/example/battleconlifetracker/model/Team.kt
+++ app/src/main/java/com/example/battleconlifetracker/model/Team.kt
@@ -1,15 +1,28 @@
 package com.example.battleconlifetracker.model
 
+import android.view.View
 import com.example.battleconlifetracker.model.player.Player
 
-class Team (vararg val playerArray: Player) {
+class Team (vararg players: Player) {
 
     private val maxForce = 20
     private var currentForce = 0
     private var forcePerBeat = 0
+    var playerArray: Array<Player> = arrayOf()
 
     init {
-        playerArray.forEach { player -> forcePerBeat += player.forcePerBeat }
+        for(i in players.indices) {
+            playerArray[i] = players[i]
+            forcePerBeat += players[i].forcePerBeat
+        }
+        updateForcePerBeat()
+    }
+
+    constructor(playerCount: Int) : this() {
+        for(i in 0 until playerCount) {
+            playerArray[i] = Player()
+            forcePerBeat += playerArray[i].forcePerBeat
+        }
         updateForcePerBeat()
     }
 
